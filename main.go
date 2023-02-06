abfa787a1ba1ee9c33361006774f121871a8cdc9
Igor Ermolaev <igor.v.e1999@gmail.com>
2023-02-06 12:33:00 +0700

    Fix "pflag: help requested", add more informative usage message

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/paraquin/dirsort/config"
@@ -18,7 +19,12 @@ func init() {
 	mappingFile := flag.StringP("set-mapping", "s", "", "move mapping JSON file to user's config directory")
 	flag.BoolVarP(&isInteractive, "interactive", "i", false, "prompt before every move")
 	flag.BoolVarP(&isVerbose, "verbose", "v", false, "explain what is being done")
+	help := flag.BoolP("help", "h", false, "display this help and exit")
 	flag.Parse()
+
+	if *help {
+		printHelp()
+	}
 
 	if *mappingFile != "" {
 		config.New(*mappingFile)
@@ -30,11 +36,18 @@ func init() {
 	}
 }
 
+// Prints help message end exit
+func printHelp() {
+	fmt.Printf("Usage: %s [OPTIONS]... DIR\n", os.Args[0])
+	fmt.Println(flag.CommandLine.FlagUsages())
+	os.Exit(0)
+}
+
 func main() {
-	dir, _ := os.Getwd()
-	if len(os.Args) > 1 {
-		dir = os.Args[1]
+	if len(flag.Args()) != 1 {
+		printHelp()
 	}
+	dir := flag.Arg(0)
 
 	m := mapper.New(mapping, isInteractive, isVerbose)
 	m.Sort(dir)
