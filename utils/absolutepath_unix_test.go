8cd6a2e6b9811bd2009320274209960fe7cb5f6b
Igor Ermolaev <igor.v.e1999@gmail.com>
2023-09-20 11:33:52 +0700

    Move TestAbsolutePath to OS specific files

---
+++ utils/absolutepath_unix_test.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+	"testing"
+)
+
+func TestAbsolutePath(t *testing.T) {
+	currentDir, _ := os.Getwd()
+	currentUser, _ := user.Current()
+	testCases := []struct {
+		desc   string
+		expect string
+		got    string
+	}{
+		{
+			desc:   "already absolute path",
+			expect: "/home/user/testdir",
+			got:    AbsolutePath("/home/user/testdir"),
+		},
+		{
+			desc:   "user home directory symbol",
+			expect: filepath.Join(currentUser.HomeDir, "Documents"),
+			got:    AbsolutePath("~/Documents"),
+		},
+		{
+			desc:   "relative path",
+			expect: filepath.Join(currentDir, "testdir"),
+			got:    AbsolutePath("testdir"),
+		},
+		{
+			desc:   "with double dot",
+			expect: filepath.Join(currentDir, "testdir"),
+			got:    AbsolutePath("dir/../testdir"),
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if tC.expect != tC.got {
+				t.Errorf("test %q: epected %q, but got %q", tC.desc, tC.expect, tC.got)
+			}
+		})
+	}
+}
---
+++ utils/absolutepath_windows_test.go
@@ -0,0 +1,39 @@
+package utils
+
+func TestAbsolutePath(t *testing.T) {
+	currentDir, _ := os.Getwd()
+	currentUser, _ := user.Current()
+	testCases := []struct {
+		desc   string
+		expect string
+		got    string
+	}{
+		{
+			desc:   "already absolute path",
+			expect: `C:\\users\user\testdir`,
+			got:    AbsolutePath(`C:\\users\user\testdir`),
+		},
+		{
+			desc:   "user home directory symbol",
+			expect: filepath.Join(currentUser.HomeDir, "Documents"),
+			got:    AbsolutePath(`~\Documents`),
+		},
+		{
+			desc:   "relative path",
+			expect: filepath.Join(currentDir, "testdir"),
+			got:    AbsolutePath("testdir"),
+		},
+		{
+			desc:   "with double dot",
+			expect: filepath.Join(currentDir, "testdir"),
+			got:    AbsolutePath(`dir\..\testdir`),
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if tC.expect != tC.got {
+				t.Errorf("test %q: epected %q, but got %q", tC.desc, tC.expect, tC.got)
+			}
+		})
+	}
+}
