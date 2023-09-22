f58496245b7643caef42da0096f3baa07c539ad7
Igor Ermolaev <igor.v.e1999@gmail.com>
2023-09-22 18:01:10 +0700

    Add new functions: MoveFile, WrapError, FileExists
    Also added callers to error messages

--- utils/utils.go
+++ utils/utils.go
@@ -2,29 +2,94 @@ package utils
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path"
 	"path/filepath"
 )
 
-// Writes an error message to os.Stderr and exit.
+// Writes an error message to os.Stderr.
 func Error(err error) {
 	fmt.Fprintln(os.Stderr, err.Error())
-	os.Exit(1)
+}
+
+func WrapError(caller string, err error) error {
+	return fmt.Errorf("%s: %w", caller, err)
 }
 
 // Copy src file content to dst
 // If dst contains not existing path, create it
 func CopyFile(src, dst string) error {
+	const op = "utils.CopyFile"
+
 	buf, err := os.ReadFile(src)
 	if err != nil {
-		return err
+		return WrapError(op, err)
 	}
+
 	if err = EnsureDirs(dst); err != nil {
-		return err
+		return WrapError(op, err)
 	}
+
 	err = os.WriteFile(dst, buf, 0644)
-	return err
+	if err != nil {
+		return WrapError(op, err)
+	}
+
+	return nil
+}
+
+// Move src file content to dst
+// If dst contains not existing path, create it
+func MoveFile(src, dst string) error {
+	const op = "utils.MoveFile"
+
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return WrapError(op, err)
+	}
+
+	buf, err := io.ReadAll(srcFile)
+	if err != nil {
+		return WrapError(op, err)
+	}
+
+	if err = EnsureDirs(dst); err != nil {
+		return WrapError(op, err)
+	}
+
+	srcStat, err := srcFile.Stat()
+	if err != nil {
+		return WrapError(op, err)
+	}
+
+	newFile, err := os.Create(dst)
+	if err != nil {
+		return WrapError(op, err)
+	}
+	defer newFile.Close()
+
+	if _, err = newFile.Write(buf); err != nil {
+		return WrapError(op, err)
+	}
+
+	newFile.Chmod(srcStat.Mode())
+	os.Chtimes(dst, srcStat.ModTime(), srcStat.ModTime())
+
+	srcFile.Close() // Need close before removing
+	if err = os.Remove(src); err != nil {
+		return WrapError(op, err)
+	}
+
+	return nil
+}
+
+func FileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
 
 // Creates necessary directories for provided filepath
